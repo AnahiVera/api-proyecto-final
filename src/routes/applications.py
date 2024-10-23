3d406ee6e4eaefe9461808294d458b5b60c20669
AnahiVera <vera.anahi.93@gmail.com>
2024-10-23 10:47:51 -0300

    route get by user applications

--- src/routes/applications.py
+++ src/routes/applications.py
@@ -20,6 +20,24 @@ def get_applications(post_id):
 
     return jsonify({"status": "success", "message": "All applications found", "applications": serialized_applications}), 200
 
+@bp_application.route('/applications/user/<int:user_id>', methods=['GET']) #para traer según el user que aplico
+@jwt_required()
+def get_user_applications(user_id):
+    current_user_id = get_jwt_identity()
+
+    if current_user_id != user_id:
+        return jsonify({"status": "error", "message": "Not authorized"}), 403
+
+    applications = Application.query.filter_by(user_id=user_id).all()
+
+    if not applications:
+        return jsonify({"status": "success", "message": "No applications found.", "applications": []}), 200
+
+    serialized_applications = [application.serialize() for application in applications]
+
+    return jsonify({"status": "success", "applications": serialized_applications}), 200
+    
+
 @bp_application.route('/applications', methods=['POST']) #aplicar a publicacion de trabajo
 @jwt_required()
 def apply_job():
@@ -100,7 +118,7 @@ def accept_application(id):
     return jsonify({"status": "success", "application": application.serialize(), "rejected_applications" : serialized_rejected}), 200
 
 
-@bp_application.route('/applications/<int:application_id>', methods=['DELETE']) #el aplicante puede eliminar su aplicacion  delete 
+@bp_application.route('/applications/<int:application_id>', methods=['DELETE']) #el aplicante puede eliminar su aplicacion  
 @jwt_required()
 def delete_application(application_id):
     user_id = get_jwt_identity()
